263d61d431c9ca803323b2685be99ebfce747a79
Wojciech Wierchoła <admin@webcarrot.pl>
2022-05-25 10:27:37 +0200

    Fixes (#9)
    * update deps
    
    * fix symbol to string
    
    * prettier
    
    * 1.8.1
    
    * update example

--- node.ts
+++ node.ts
@@ -9,65 +9,71 @@ import {
 
 import { makeAbortError, makeError } from "./errors";
 
-export const makeApi = <Data extends ApiData, Context>({
-  actions,
-  context,
-}: {
-  actions: Data;
-  context: Context;
-}): ApiResolver<Data> => <N extends keyof Data>(
-  action: N,
-  payload: Payload<Data[N]>
-): PromiseA<Unpacked<Output<Data[N]>>> => {
-  let aborted = false;
-  const promise = new Promise<Unpacked<Output<Data[N]>>>((resolve, reject) => {
-    if (aborted) {
-      reject(makeAbortError(action));
-    } else if (
-      action in actions &&
-      actions.hasOwnProperty(action) &&
-      typeof actions[action] === "function"
-    ) {
-      try {
-        const output = actions[action](payload, context);
-        if (output instanceof Promise) {
-          output.then(
-            (data: Unpacked<Output<Data[N]>>) => {
+export const makeApi =
+  <Data extends ApiData, Context>({
+    actions,
+    context,
+  }: {
+    actions: Data;
+    context: Context;
+  }): ApiResolver<Data> =>
+  <N extends keyof Data>(
+    action: N,
+    payload: Payload<Data[N]>
+  ): PromiseA<Unpacked<Output<Data[N]>>> => {
+    let aborted = false;
+    const promise = new Promise<Unpacked<Output<Data[N]>>>(
+      (resolve, reject) => {
+        if (aborted) {
+          reject(makeAbortError(action));
+        } else if (
+          action in actions &&
+          actions.hasOwnProperty(action) &&
+          typeof actions[action] === "function"
+        ) {
+          try {
+            const output = actions[action](payload, context);
+            if (output instanceof Promise) {
+              output.then(
+                (data: Unpacked<Output<Data[N]>>) => {
+                  if (!aborted) {
+                    resolve(data);
+                  } else {
+                    reject(makeAbortError(action));
+                  }
+                },
+                (err: any) =>
+                  reject(
+                    aborted ? makeAbortError(action) : makeError(err, action)
+                  )
+              );
+            } else {
               if (!aborted) {
-                resolve(data);
+                resolve(output);
               } else {
                 reject(makeAbortError(action));
               }
-            },
-            (err: any) =>
-              reject(aborted ? makeAbortError(action) : makeError(err, action))
-          );
-        } else {
-          if (!aborted) {
-            resolve(output);
-          } else {
-            reject(makeAbortError(action));
+            }
+          } catch (err) {
+            reject(aborted ? makeAbortError(action) : makeError(err, action));
           }
+        } else {
+          reject(
+            makeError(
+              {
+                message: `Unknown action "${String(action)}"`,
+                name: "ActionUnknown",
+              },
+              action
+            )
+          );
         }
-      } catch (err) {
-        reject(aborted ? makeAbortError(action) : makeError(err, action));
       }
-    } else {
-      reject(
-        makeError(
-          {
-            message: `Unknown action "${action}"`,
-            name: "ActionUnknown",
-          },
-          action
-        )
-      );
-    }
-  });
-  Object.defineProperty(promise, "abort", {
-    value: () => {
-      aborted = true;
-    },
-  });
-  return promise;
-};
+    );
+    Object.defineProperty(promise, "abort", {
+      value: () => {
+        aborted = true;
+      },
+    });
+    return promise;
+  };
